4f6c0cbc249bd9f8e3f700e343545b39b83deaec
Zergulina <antonchernyaevt@gmail.com>
2024-12-06 08:26:23 +0300

    light theme

--- src/UI/DropDown/DropDown.jsx
+++ src/UI/DropDown/DropDown.jsx
@@ -4,7 +4,7 @@ import classes from './DropDown.module.css'
 const DropDown = ({placeholder, options, selectedOption, setSelectedOption, className}) => {
     return (
         <select value={selectedOption} onChange={setSelectedOption} className={`${classes.DropDown} ${className}`}>
-            <option value="">{placeholder}</option>
+            <option selected hidden value="" >{placeholder}</option>
             {
                 options.map((option) => 
                     <option key={option.id} value={option.id}>
